7e8368e8df229cacb6cbc7529c88bff8dbdef54f
jincheng.zhou <jincheng.zhou@sumscope.com>
2020-05-17 21:05:39 +0800

    提交第三方库

--- Test/MongodbTest/main.cpp
+++ Test/MongodbTest/main.cpp
@@ -1,8 +1,12 @@
 #include <QtCore/QCoreApplication>
+#include <QDebug>
+#include <mongocxx/client.hpp>
 
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
+
+	qDebug() << "Hello...";
 
 	return a.exec();
 }
